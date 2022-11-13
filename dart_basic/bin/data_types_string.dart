6cb6afac8307eb30845994a25089bb62dec51b24
chrystalio <chrystalio.k@outlook.com>
2022-11-13 19:24:51 +0700

    Add string data type on dart

---
+++ dart_basic/bin/data_types_string.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Tipe data string
+  // Tidak boleh dimulai dengan angka
+  // Tidak boleh mengandung spasi
+  // Tidak boleh diawali simbol kecuali underscore
+
+  String namaLengkap = 'Bambang';
+  String _alamat;
+  const String namaBelakang = 'Surya';
+
+  _alamat = 'Jl. Kebon Jeruk No. 1';
+
+  String _namaBelakang = 'Saputra';
+
+  print(namaLengkap);
+  print(namaBelakang);
+  print(_alamat);
+}
